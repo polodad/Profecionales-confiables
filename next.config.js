b73074f7c0a6e9a79a8a497a49cf49df7468dee4
Tu Nombre <tu-email@ejemplo.com>
2025-10-14 00:29:43 -0600

    security: elimina JWT expuesto y migra a variables de entorno

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: 'https://ufxbrtvkhibstxniumbe.supabase.co',
-    NEXT_PUBLIC_SUPABASE_ANON_KEY:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmeGJydHZraGlic3R4bml1bWJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAzOTUzNzUsImV4cCI6MjA3NTk3MTM3NX0.c8qA9SiEuHAy4h4umuvG-8qrtVzDzoF0aS4j5ac9mi4',
-  },
   images: {
     remotePatterns: [
       {
